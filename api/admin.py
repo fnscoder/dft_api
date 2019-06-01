c179ebf62ba212c3c027528aba3da5e954864620
fnscoder <fnscoder@gmail.com>
2019-06-01 18:19:39 -0300

    Added list_display to  admin

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Shoe
 
 
-admin.site.register(Shoe)
+class ShoeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'brand', 'color', 'size', 'price', 'quantity', 'created_at', 'updated_at')
+    
+
+admin.site.register(Shoe, ShoeAdmin)
