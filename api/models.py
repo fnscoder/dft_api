cc4b953a108dbf52698b6ff3d8270a08d7b4a6e3
fnscoder <fnscoder@gmail.com>
2019-06-01 18:10:21 -0300

    Updated model description

--- api/models.py
+++ api/models.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 
 class Shoe(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    brand = models.CharField(max_length=100)
-    color = models.CharField(max_length=20)
-    size = models.IntegerField()
-    price = models.DecimalField(decimal_places=2, max_digits=6)
-    quantity = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
+    brand = models.CharField('Marca', max_length=100)
+    color = models.CharField('Cor', max_length=20)
+    size = models.IntegerField('Tamanho')
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=6)
+    quantity = models.IntegerField('Quantidade')
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'calçados'
         verbose_name = 'calçado'
+        verbose_name_plural = 'calçados'
 
     def __str__(self):
         return self.brand
