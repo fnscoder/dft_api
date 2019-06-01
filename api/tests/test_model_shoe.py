1c47d9712c284037c4c64fa45ac7d63a30986c94
fnscoder <fnscoder@gmail.com>
2019-06-01 17:49:51 -0300

    Created model tests

---
+++ api/tests/test_model_shoe.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from ..models import Shoe
+
+
+class ShoeModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="dafiti",
+            email="dafiti@email.com",
+            password="dafitipass",
+        )
+        self.obj = Shoe(
+            user_id=self.user.pk,
+            brand='nike',
+            color='white',
+            size=40,
+            price=199.90,
+            quantity=5,
+        )
+        self.obj.save()
+
+    def test_create(self):
+        """Shoe object must exist"""
+        self.assertTrue(Shoe.objects.exists())
+
+    def test_created_at(self):
+        """Shoe must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        """str method must returns shoe's brand"""
+        self.assertEqual('nike', str(self.obj))
