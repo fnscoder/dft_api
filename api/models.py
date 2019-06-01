e4a438c4a4cae7d6c9339215a3d25b3cbedf77c2
fnscoder <fnscoder@gmail.com>
2019-06-01 17:53:19 -0300

    Created model and added to admin

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Shoe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    brand = models.CharField(max_length=100)
+    color = models.CharField(max_length=20)
+    size = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+
+    class Meta:
+        verbose_name_plural = 'calçados'
+        verbose_name = 'calçado'
+
+    def __str__(self):
+        return self.brand
