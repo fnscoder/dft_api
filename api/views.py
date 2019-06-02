22cdf1d119f58994f94e750cbacf917b69185be8
fnscoder <fnscoder@gmail.com>
2019-06-01 22:17:14 -0300

    Created views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+
+from .models import Shoe
+from .serializers import ShoeSerializer
+
+
+class ShoeView(ListCreateAPIView):
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
+
+
+class SingleShoeView(RetrieveUpdateDestroyAPIView):
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
