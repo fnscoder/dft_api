70c285fe153b76d0b6c23a87080cd51be5fe5d4e
fnscoder <fnscoder@gmail.com>
2019-06-02 15:37:10 -0300

    Implemented filter backend

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
@@ -12,6 +13,8 @@ from .serializers import ShoeSerializer
 class ShoeView(ListCreateAPIView):
     queryset = Shoe.objects.get_queryset().order_by('id')
     serializer_class = ShoeSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("brand", "color", "size", "price", "quantity")
 
 
 class SingleShoeView(RetrieveUpdateDestroyAPIView):
--- dft_api/settings.py
+++ dft_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_filters',
     'rest_framework',
     'api',
 ]
@@ -131,7 +132,9 @@ REST_FRAMEWORK = {
         "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",)
+    
 }
 
 JWT_AUTH = {"JWT_EXPIRATION_DELTA": datetime.timedelta(seconds=300)}
