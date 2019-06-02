a0b1f8d7318299a3b152c603b005161d689c295b
fnscoder <fnscoder@gmail.com>
2019-06-02 15:36:43 -0300

    Added filter tests and improve other tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -48,18 +48,23 @@ class ShoesTest(APITestCase):
         
         self.post_response = self.client.post("/api/shoes/", shoe_data, format="json")
         self.get_all_response = self.client.get("/api/shoes/")
+        self.get_filter_response = self.client.get("/api/shoes/?brand=nike")
         self.get_one_response = self.client.get("/api/shoes/1/")
         self.put_response = self.client.put("/api/shoes/1/", new_data, format="json")
         self.patch_response = self.client.patch("/api/shoes/1/", partial_data, format="json")
         self.delete_response = self.client.delete("/api/shoes/1/")
 
     def test_post_shoes(self):
+        """Must return status code 201 created"""
         self.assertEqual(self.post_response.status_code, status.HTTP_201_CREATED)
 
     def test_get_all_shoes(self):
+        """Must return status code 200 and contains shoe created nike"""
         self.assertEqual(self.get_all_response.status_code, status.HTTP_200_OK)
+        self.assertContains(self.get_all_response, "nike")
 
     def test_pagination_shoes(self):
+        """Must contains dict content for pagination"""
         contents = ("count",
                     "next",
                     "previous",
@@ -67,16 +72,28 @@ class ShoesTest(APITestCase):
                 )
         for content in contents:
             with self.subTest():
-                self.assertContains(self.get_all_response, content)        
+                self.assertContains(self.get_all_response, content)
+
+    def test_filter_shoes(self):
+        """Must return status code 200 and contains response for filter brand=nike"""
+        self.assertEqual(self.get_filter_response.status_code, status.HTTP_200_OK)
+        self.assertContains(self.get_filter_response, "nike")
 
     def test_get_one_shoe(self):
+        """Must return status code 200 and contains the content created with id=1"""
         self.assertEqual(self.get_one_response.status_code, status.HTTP_200_OK)
+        self.assertContains(self.get_one_response, "nike")
 
     def test_put_shoe(self):
+        """Must return status code 200 and data changed for shoe with id=1"""
         self.assertEqual(self.put_response.status_code, status.HTTP_200_OK)
+        self.assertContains(self.put_response, "adidas")
 
     def test_patch_shoe(self):
+        """Must return status code 200 and quantity changed for shoe with id=1"""
         self.assertEqual(self.patch_response.status_code, status.HTTP_200_OK)
+        self.assertContains(self.patch_response, "10")
 
     def test_delete_shoe(self):
+        """Must return status code 204 no content data"""
         self.assertEqual(self.delete_response.status_code, status.HTTP_204_NO_CONTENT)
